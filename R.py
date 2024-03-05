1f58909962e2afed24fda9ef2cc2bddc315fce6a
jasca072 <162274402+jasca072@users.noreply.github.com>
2024-03-05 09:43:11 +0300

    R.py
    asdassdsa

---
+++ R.py
@@ -0,0 +1,26 @@
+print('Men qila olaman!!!')
+print('Assalomu aleykum Dunyo, bu O\'zbekistaon')
+# \ Bu kod
+
+print('*')
+print('**')
+print('***')
+print('****')
+print('*****')
+print('******')
+
+# \ Bu kod
+
+a = 20
+print('juft sonlar 20 gacha yoki toq sonlar 19 gacha')
+son = input('juft sonlar uchun = 2 va yoq sonlar uchun = 3 ni yoz!~!')
+if son == '2':
+    for i in range(2, a, 2):
+        print(i)
+        i = 2
+        i += 2
+elif son == '3':
+    for i in range(3, a, 2):
+        print(i)
+        i = 2
+        i += 2
